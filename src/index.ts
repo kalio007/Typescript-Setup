d803391fc8a64b88479e88f8c9b60c3700023945
Kalio007 <Kalioprince1@gmail.com>
2023-02-22 23:22:54 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+let sales = 123456789;
+let course = 'typescript';
+let is_published = true;
+let level;
+// Ensuring a type for a variable if it wasnt declared earlier
+function render(document: any){
+    console.log(document)
+}
+// Arrays
+let numbers: number[] = [1, 2, '3']
